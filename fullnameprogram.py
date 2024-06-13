03794de4fa7f3f4b3c6fc325dfa9738f27685065
nikki <nikithareddy8431@gmail.com>
2024-06-13 13:56:10 -0500

    inital Commit

---
+++ fullnameprogram.py
@@ -0,0 +1,13 @@
+name1= 'vineeth Reddy';
+name2='Jallapuram';
+fullname=name1+name2;
+print(fullname);
+print(fullname +'Indian');
+print('Ashok Jallapuram father of'+ fullname);
+print(len(name1)+len(name2));
+print(len(fullname));
+print(name1[4:]);
+print(name2[3:5]);
+print(name1[-4]);
+print(name2[-3]);
+
